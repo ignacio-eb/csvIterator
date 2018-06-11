fc5e05a8f7cc54dabfebd9be19cb740e46a02826
ignacio-eb <ignacio@eventbrite.com>
2018-06-11 17:07:43 -0300

    Adding some changes to support current and expected URL

--- csvIterator/csvReader.py
+++ csvIterator/csvReader.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import csv
 from selenium import webdriver
 
@@ -28,7 +29,8 @@ class CSVReader():
 
             for row in urlReader:
                 baseUrl = str(row[0])
-                print("Base URL is: ",baseUrl, " || Site Nº",count, "of ", total, " sites")
+                expected = str(row[1])
+                print("Source and Target URLs are: ", row, " || Site #",count, "of ", total, " sites")
                 count = count + 1
                 driver = webdriver.Firefox()
                 driver.implicitly_wait(10)
@@ -38,22 +40,23 @@ class CSVReader():
                 print ("Current URL is: ", current)
 
                 #compare URLs are equal
-                self.compare_url(baseUrl,current)
+                self.compare_url(expected,current)
+                print("*" * 100)
 
                 #Check that BUY Ticket button is visible
-                try:
-                    buy_button = driver.find_element_by_xpath("//div[@id='root']/div//div[@role='region']//div[@class='eds-structure__main-mask']/div[@class='eds-structure__fixed-bottom-bar-layout-wrapper']/div[@class='eds-fixed-bottom-bar-layout']/div[@class='eds-fixed-bottom-bar-layout__content']/div[@class='eds-structure__main-container']/main[@class='eds-structure__main']//div[@class='eds-l-mar-bot-8']//section[@class='eds-carousel']/div[@class='eds-show-up-sw']/div/div[@class='eds-base-carousel']/div[@class='eds-align--center-vertical eds-align--space-around']/div[@class='eds-align--center-horizontal']//div[@class='featured-events-carousel-info']/div/div[2]//a[contains(text(),'BUY TICKETS')]")
-                    if buy_button.is_displayed():
-                        print("Buy Tickets for " + baseUrl + " is visible")
-                        print("*" * 100)
-                    else:
-                        print("Buy Tickets for " + baseUrl + " is NOT visible")
-                        print("*" * 100)
-                except:
-                    print("NO BUTTON FOUND")
-                    print("*"*100)
-                    driver.close()
-                    continue
+                # try:
+                #     buy_button = driver.find_element_by_xpath("//div[@id='root']/div//div[@role='region']//div[@class='eds-structure__main-mask']/div[@class='eds-structure__fixed-bottom-bar-layout-wrapper']/div[@class='eds-fixed-bottom-bar-layout']/div[@class='eds-fixed-bottom-bar-layout__content']/div[@class='eds-structure__main-container']/main[@class='eds-structure__main']//div[@class='eds-l-mar-bot-8']//section[@class='eds-carousel']/div[@class='eds-show-up-sw']/div/div[@class='eds-base-carousel']/div[@class='eds-align--center-vertical eds-align--space-around']/div[@class='eds-align--center-horizontal']//div[@class='featured-events-carousel-info']/div/div[2]//a[contains(text(),'BUY TICKETS')]")
+                #     if buy_button.is_displayed():
+                #         print("Buy Tickets for " + baseUrl + " is visible")
+                #         print("*" * 100)
+                #     else:
+                #         print("Buy Tickets for " + baseUrl + " is NOT visible")
+                #         print("*" * 100)
+                # except:
+                #     print("NO BUTTON FOUND")
+                #     print("*"*100)
+                #     driver.close()
+                #     continue
 
                 #close window
                 driver.close()
